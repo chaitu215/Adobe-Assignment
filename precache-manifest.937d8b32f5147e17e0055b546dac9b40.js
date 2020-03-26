855033def7978947814ae7298708b463122bf725
chaitu215 <krishna.mf01@gmail.com>
2020-03-26 12:02:49 +0530

    Updates

---
+++ precache-manifest.937d8b32f5147e17e0055b546dac9b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea7c30e4de6af63d91724b25e7c1fe4",
+    "url": "/Adobe-Assignment/index.html"
+  },
+  {
+    "revision": "c0b8fae77ba4ebc2e363",
+    "url": "/Adobe-Assignment/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4f0cf68df00ed275cdaf",
+    "url": "/Adobe-Assignment/static/css/main.980ad389.chunk.css"
+  },
+  {
+    "revision": "c0b8fae77ba4ebc2e363",
+    "url": "/Adobe-Assignment/static/js/2.203d0594.chunk.js"
+  },
+  {
+    "revision": "4f0cf68df00ed275cdaf",
+    "url": "/Adobe-Assignment/static/js/main.d08efabb.chunk.js"
+  },
+  {
+    "revision": "8790940775c5d44c7aa4",
+    "url": "/Adobe-Assignment/static/js/runtime~main.cce9d1d5.js"
+  },
+  {
+    "revision": "32535d5f33af000e182d71c1cd15ee00",
+    "url": "/Adobe-Assignment/static/media/star.32535d5f.jpg"
+  }
+]);
